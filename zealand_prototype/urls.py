11c56c1f6a77493965ac8d144c7ad6801379bb2b
sj <nabutech@proton.me>
2024-08-23 14:01:07 +1200

    Authentication

--- zealand_prototype/urls.py
+++ zealand_prototype/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),  # This includes the frontend app's URLs
+    path('auth/', include('authentication.urls')),
 ]
